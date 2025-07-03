ce5c181568fd713bd314e70a1a08f1898b2cac84
skysi <285922179@qq.com>
2025-07-03 14:17:27 +0800

    add commonmanager 1.0.0

---
+++ src/main/java/android/os/common/MyContent.java
@@ -0,0 +1,12 @@
+package android.os.common;
+
+public class MyContent {
+    public static final String COMMON="common";
+    /**
+     * 0 默认
+     * 1 PCBA Yes
+     * 2 压测ok
+     * 3 整机ok
+     */
+    public static int KEY_PRODUCTION_TESTING_ID= 36;
+}
